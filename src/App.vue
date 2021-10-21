d4e2d649f7af25ef3e6c364cdd6e2bc46dd18177
yetrun <89437145+yetrun@users.noreply.github.com>
2021-10-21 15:16:24 +0800

    引入 'json5-to-table' 并生成 HTML Table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
+  <!-- 左右分开，一边是 JSON 源代码，一边是表格 -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <textarea v-model="dataText" />
+    </div>
+    <div>
+      <textarea v-model="schemaText" />
+    </div>
+    <button @click="generateTableHTML">生成</button>
+    <div v-html="tableHTML"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { generateHTMLTable } from 'json5-to-table'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      dataText: JSON.stringify([
+        { a: 1, b: 2 },
+        { a: 3, b: 4 }
+      ]),
+      schemaText: '',
+      tableHTML: 'HTML Table 将展示在此'
+    }
+  },
+  computed: {
+    dataObject () {
+      try {
+        return JSON.parse(this.dataText)
+      } catch (ex) {
+        if (ex instanceof SyntaxError) {
+          return []
+        } else {
+          throw ex
+        }
+      }
+    },
+    schemaObject () {
+      try {
+        return JSON.parse(this.schemaText)
+      } catch (ex) {
+        if (ex instanceof SyntaxError) {
+          return null
+        } else {
+          throw ex
+        }
+      }
+    }
+  },
+  methods: {
+    generateTableHTML () {
+      this.tableHTML = generateHTMLTable(this.dataObject, this.schemaObject)
+    }
   }
 }
 </script>
 
-<style lang="scss">
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
+
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  display: flex;
+
+  > div {
+    width: 50%;
+
+    textarea {
+      width: 100%;
+    }
+  }
 }
 </style>
